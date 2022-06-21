fb9e9cd32bc30728b7d5d2541394c08e7fe99e50
mizzy5 <100967807+mizzy5@users.noreply.github.com>
2022-06-21 17:43:58 +0100

    first commit

--- I4G013578FOF/blog/models.py
+++ I4G013578FOF/blog/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
 # Create your models here.
 class Post(models.Model):
@@ -8,11 +9,6 @@ class Post(models.Model):
     author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
     created_date = models.DateTimeField(auto_now=False, auto_now_add=True)
     published_date = models.DateTimeField(auto_now=True, auto_now_add=False)
-
-    class Meta:
-        db_table = ''
-        managed = True
-        verbose_name_plural = 'Posts'
 
     def __str__(self) -> str:
         return (f"{self.title} by {self.author}").__str__()
